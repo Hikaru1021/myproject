cbda321538f9c7a15febcf95a26f4fef3ce1c10b
Hikaru1021 <kagayaki.kirari.1021@gmail.com>
2024-02-25 16:41:42 +0900

    ログイン追加

--- myproject/settings.py
+++ myproject/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media_local'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'list'
+LOGOUT_REDIRECT_URL = 'top'
